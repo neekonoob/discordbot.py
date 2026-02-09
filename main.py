a912b8455b01091839c346f76cb058b42513a4b8
notnotneeko <conghoang27586@gmail.com>
2026-02-09 12:09:08 +0700

    bug fix

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
+import threading
 import discord
 import requests
 from discord.ext import commands 
 import logging 
 from dotenv import load_dotenv
+from flask import Flask
 import os 
+#web server 
+app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "Bot is alive! 👀"
+
+def run_flask():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
+if not token:
+    print("❌ DISCORD_TOKEN is missing")
+    SystemExit(1)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
-
 bot = commands.Bot(command_prefix='!', intents= intents)
+
 
 @bot.event
 async def on_ready() :
@@ -50,5 +64,7 @@ async def weather(ctx, *, city: str):
         f"Thời tiết: {desc}"
     )
     await ctx.send(result)
-        
+if __name__ == "__main__":
+    threading.Thread(target=run_flask, daemon=True).start()
+    bot.run(token)
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
